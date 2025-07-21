c4ab417ba211dd4f056ffe9dfb790cd6a69713f7
Allen Robel <arobel@cisco.com>
2025-07-21 08:05:55 -1000

    Copy local disk images to /iso/nxos
    Am going to try this change in one file.  If it works, will update the other files.

--- n9kv_install_ER.sh
+++ n9kv_install_ER.sh
@@ -2,17 +2,18 @@
 
 # Install multiple N9KV instances using your TAP topology
 # Based on working QEMU configuration
-N9KV_IMAGE="/iso/nxos/nexus9300v64.10.3.8.M.qcow2"
-
+IMAGE_PATH="/iso/nxos"
+N9KV_SHARED_IMAGE="$IMAGE_PATH/nexus9300v64.10.3.8.M.qcow2"
+N9KV_PRIVATE_IMAGE="$IMAGE_PATH/ER.qcow2"
 # Create local disk copies for each VM (N9KV needs writable disks)
-cp "$N9KV_IMAGE" ER.qcow2
+cp "$N9KV_SHARED_IMAGE" "$N9KV_PRIVATE_IMAGE"
 
 # Switch 1 (ER) - Edge Router
 virt-install \
     --name="n9kv-ER" \
     --machine q35 \
     --ram=8192 --vcpus=4 \
-    --disk path=ER.qcow2,format=qcow2,bus=sata \
+    --disk path=$N9KV_PRIVATE_IMAGE,format=qcow2,bus=sata \
     --network type=direct,source=tap_ER_MGMT,model=e1000,mac=00:b0:21:02:00:00 \
     --network type=direct,source=tap_ER_E1_1,model=e1000,mac=00:b0:21:02:00:01 \
     --network type=direct,source=tap_ER_E1_2,model=e1000,mac=00:b0:21:02:00:02 \

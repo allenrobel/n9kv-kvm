87017bd1154261ca815ae0557a4b55623ad77734
Allen Robel <arobel@cisco.com>
2025-07-20 17:36:46 -1000

    chmod x

--- n9kv_install.sh
+++ n9kv_install.sh
@@ -5,7 +5,7 @@ N9KV_IMAGE="/iso/nxos/nexus9300v64.10.3.8.M.qcow2"
 
 # Switch 1 (ER) - Edge Router
 virt-install \
-    --name="n9kv-er1" \
+    --name="n9kv-ER" \
     --ram=8192 --vcpus=4 \
     --disk path="$N9KV_IMAGE",format=qcow2,bus=ide,cache=writethrough \
     --network type=ethernet,source.dev=tap_ER_MGMT,model=e1000 \
@@ -16,7 +16,7 @@ virt-install \
 
 # Switch 2 (S1) - Spine Switch
 virt-install \
-    --name="n9kv-spine1" \
+    --name="n9kv-S1" \
     --ram=8192 --vcpus=4 \
     --disk path="$N9KV_IMAGE",format=qcow2,bus=ide,cache=writethrough \
     --network type=ethernet,source.dev=tap_S1_MGMT,model=e1000 \
@@ -27,7 +27,7 @@ virt-install \
 
 # Switch 3 (S2) - Spine Switch
 virt-install \
-    --name="n9kv-spine2" \
+    --name="n9kv-S2" \
     --ram=8192 --vcpus=4 \
     --disk path="$N9KV_IMAGE",format=qcow2,bus=ide,cache=writethrough \
     --network type=ethernet,source.dev=tap_S2_MGMT,model=e1000 \
@@ -38,7 +38,7 @@ virt-install \
 
 # Switch 4 (L1) - Leaf Switch
 virt-install \
-    --name="n9kv-leaf1" \
+    --name="n9kv-L1" \
     --ram=8192 --vcpus=4 \
     --disk path="$N9KV_IMAGE",format=qcow2,bus=ide,cache=writethrough \
     --network type=ethernet,source.dev=tap_L1_MGMT,model=e1000 \
@@ -49,10 +49,10 @@ virt-install \
 
 
 echo "All N9KV instances created. Network topology:"
-echo "ER ←→ Spine1: BR_ER_S1"
-echo "ER ←→ Spine2: BR_ER_S2"
-echo "Leaf1 ←→ Spine1: BR_S1_L1"
-echo "Leaf1 ←→ Spine2: BR_S1_L2"
+echo "ER -> S1: BR_ER_S1"
+echo "ER -> S2: BR_ER_S2"
+echo "S1 -> L1: BR_S1_L1"
+echo "S2 -> L1: BR_S2_L1"
 echo "All mgmt interfaces on ndfc-mgmt bridge"
 echo "If Virtual Machine Manager (virt-manager) is installed, you can view and manage these instances there."
 echo "See env in this directory if virsh list is not displaying VM instances."

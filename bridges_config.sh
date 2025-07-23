d02d7f8b771d2607829d976231bdb8e3cbc204c8
Allen Robel <arobel@cisco.com>
2025-07-23 09:27:06 -1000

    Logic improvements

--- bridges_config.sh
+++ bridges_config.sh
@@ -6,18 +6,22 @@ for bridge in "${BRIDGES[@]}"; do
     if ! ip link show $bridge >/dev/null 2>&1; then
         echo "Bridge $bridge does not exist. Creating..."
         ip link add name $bridge type bridge
-        ip link set $bridge mtu 9216
-        ip link set dev $bridge arp on
-        ip link set dev $bridge promisc on
-        ip link set dev $bridge multicast on
-        ip link set dev $bridge type bridge stp_state 0
-        ip link set $bridge up
     else
         echo "Bridge $bridge already exists."
     fi
 done
 
-# Join bridges with "|" to create a single string for grep below
+for bridge in "${BRIDGES[@]}"; do
+    echo "Configuring bridge $bridge..."
+    ip link set dev $bridge mtu 9216
+    ip link set dev $bridge arp on
+    ip link set dev $bridge promisc off
+    ip link set dev $bridge multicast on
+    echo "Bringing up bridge $bridge..."
+    ip link set $bridge up
+done
+
+# Join BRIDGES with "|" to create a single string for grep below
 SAVED_IFS="$IFS"  # Save the original IFS
 IFS="|"
 JOINED_BRIDGES="${BRIDGES[*]}"
@@ -28,7 +32,8 @@ for tap in $TAPS; do
     echo "Configuring TAP interface $tap..."
     ip link set $tap mtu 9216
     ip link set dev $tap arp on
-    ip link set dev $tap promisc on
+    ip link set dev $tap promisc off
     ip link set dev $tap multicast on
+    echo "Bringing up TAP interface $tap..."
     ip link set $tap up
 done

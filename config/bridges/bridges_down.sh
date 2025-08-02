1f9241bbd7ab09a780a9e3db631c1067fe5a29c8
Allen Robel <arobel@cisco.com>
2025-08-02 09:55:44 -1000

    New topo: update bridge shell scripts

--- config/bridges/bridges_down.sh
+++ config/bridges/bridges_down.sh
@@ -28,7 +28,7 @@ ip link set tap_S2_E1_2 nomaster
 ip link set tap_L1_E1_1 nomaster
 ip link set tap_L1_E1_2 nomaster
 
-# Remove management interfaces from ndfc-mgmt bridge
+# Remove management interfaces from BR_ND_DATA bridge
 ip link set tap_ER_MGMT nomaster
 ip link set tap_S1_MGMT nomaster
 ip link set tap_S2_MGMT nomaster
@@ -39,12 +39,16 @@ ip link set BR_ER_S1 down
 ip link set BR_ER_S2 down
 ip link set BR_S1_L1 down
 ip link set BR_S2_L1 down
+ip link set BR_L1_H1 down
+ip link set BR_L2_H2 down
 
-# Delete all bridges (note: we don't delete ndfc-mgmt as it's existing)
+# Delete all bridges
 ip link delete BR_ER_S1
 ip link delete BR_ER_S2
 ip link delete BR_S1_L1
 ip link delete BR_S2_L1
+ip link delete BR_L1_H1
+ip link delete BR_L2_H2
 
 # Delete all TAP interfaces
 ip link delete tap_ER_MGMT
@@ -61,5 +65,4 @@ ip link delete tap_S2_E1_2
 ip link delete tap_L1_E1_1
 ip link delete tap_L1_E1_2
 
-echo "All network interfaces torn down successfully using modern ip commands"
-echo "Note: ndfc-mgmt bridge left intact (pre-existing)"
+echo "All network interfaces torn down successfully"

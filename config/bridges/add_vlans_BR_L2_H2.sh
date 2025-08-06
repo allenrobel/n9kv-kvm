a333ee750e0e2822311aa0fd7493f39fb2ccdfac
Allen Robel <arobel@cisco.com>
2025-08-06 10:25:28 -1000

    Scripts to add vlans to bridges

---
+++ config/bridges/add_vlans_BR_L2_H2.sh
@@ -0,0 +1,33 @@
+# Add vlans 2 and 3 to BR_L2_H2 and associated interfaces
+VLANS=(2 3)
+BRIDGE="BR_L2_H2"
+TAPS=$(bridge link show | grep -E $BRIDGE | grep tap | cut -d: -f2 | cut -d' ' -f2)
+VNETS=$(bridge link show | grep -E $BRIDGE | grep vnet | cut -d: -f2 | cut -d' ' -f2 | cut -d'@' -f1)
+
+# Exit if bridge does not exist
+if ! ip link show $BRIDGE >/dev/null 2>&1; then
+    echo "Bridge $BRIDGE does not exist. Exiting..."
+    exit 1
+fi
+
+# Add vlans to bridge
+for vlan in "${VLANS[@]}"; do
+    echo "Adding VLAN $vlan to $BRIDGE..."
+    bridge vlan add dev $BRIDGE vid $vlan
+done
+
+# Add vlans to associated TAP interfaces
+for tap in $TAPS; do
+    for vlan in "${VLANS[@]}"; do
+        echo "Adding VLAN $vlan to $tap..."
+        bridge vlan add dev $tap vid $vlan
+    done
+done
+
+# Add vlans to associated VNET interfaces
+for vnet in $VNETS; do
+    for vlan in "${VLANS[@]}"; do
+        echo "Adding VLAN $vlan to $vnet..."
+        bridge vlan add dev $vnet vid $vlan
+    done
+done

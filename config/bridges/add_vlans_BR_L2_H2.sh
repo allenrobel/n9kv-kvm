3479c4110f5d92a496caae56ba5f73f755045a25
Allen Robel <arobel@cisco.com>
2025-08-06 13:46:16 -1000

    Revert last commits

--- config/bridges/add_vlans_BR_L2_H2.sh
+++ config/bridges/add_vlans_BR_L2_H2.sh
@@ -2,7 +2,7 @@
 VLANS=(2 3)
 BRIDGE="BR_L2_H2"
 TAPS=$(bridge link show | grep -E $BRIDGE | grep tap | cut -d: -f2 | cut -d' ' -f2)
-# VNETS=$(bridge link show | grep -E $BRIDGE | grep vnet | cut -d: -f2 | cut -d' ' -f2 | cut -d'@' -f1)
+VNETS=$(bridge link show | grep -E $BRIDGE | grep vnet | cut -d: -f2 | cut -d' ' -f2 | cut -d'@' -f1)
 
 # Exit if bridge does not exist
 if ! ip link show $BRIDGE >/dev/null 2>&1; then
@@ -26,11 +26,11 @@ for tap in $TAPS; do
     done
 done
 
-# # Add vlans to associated VNET interfaces
-# for vnet in $VNETS; do
-#     for vlan in "${VLANS[@]}"; do
-#         echo "Adding VLAN $vlan to $vnet..."
-#         echo "sudo bridge vlan add vid $vlan dev $vnet"
-#         sudo bridge vlan add vid $vlan dev $vnet
-#     done
-# done
+# Add vlans to associated VNET interfaces
+for vnet in $VNETS; do
+    for vlan in "${VLANS[@]}"; do
+        echo "Adding VLAN $vlan to $vnet..."
+        echo "sudo bridge vlan add vid $vlan dev $vnet"
+        sudo bridge vlan add vid $vlan dev $vnet
+    done
+done

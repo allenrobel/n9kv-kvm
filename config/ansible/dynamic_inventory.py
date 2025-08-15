1c2419ab7d8630085e4d7ab03da32bfd186f3b87
Allen Robel <arobel@cisco.com>
2025-08-14 15:59:12 -1000

    Update switch, bridge names to reflect new topology

--- config/ansible/dynamic_inventory.py
+++ config/ansible/dynamic_inventory.py
@@ -27,19 +27,23 @@ from os import environ
 
 # Device IP addresses
 
-ND_IP4 = environ.get("ND_IP4", "192.168.11.2")
-ER_IP4 = environ.get("ER_IP4", "192.168.12.111")
-S1_IP4 = environ.get("S1_IP4", "192.168.12.121")
-S2_IP4 = environ.get("S2_IP4", "192.168.12.122")
-L1_IP4 = environ.get("L1_IP4", "192.168.12.131")
-L2_IP4 = environ.get("L2_IP4", "192.168.12.132")
-L1_IP4_INTERFACE_2 = environ.get("L1_IP4_INTERFACE_2", "11.1.1.1")
-L2_IP4_INTERFACE_2 = environ.get("L2_IP4_INTERFACE_2", "22.1.1.1")
+ND_IP4 = environ.get("ND_IP4", "192.168.7.7")
+CR1_IP4 = environ.get("CR1_IP4", "192.168.12.111")
+ER1_IP4 = environ.get("ER1_IP4", "192.168.12.121")
+BG1_IP4 = environ.get("BG1_IP4", "192.168.12.131")
+BG2_IP4 = environ.get("BG2_IP4", "192.168.12.132")
+SP1_IP4 = environ.get("SP1_IP4", "192.168.12.141")
+SP2_IP4 = environ.get("SP2_IP4", "192.168.12.142")
+LE1_IP4 = environ.get("LE1_IP4", "192.168.12.151")
+LE2_IP4 = environ.get("LE2_IP4", "192.168.12.152")
+LE1_IP4_INTERFACE_2 = environ.get("LE1_IP4_INTERFACE_2", "192.168.0.151")
+LE2_IP4_INTERFACE_2 = environ.get("LE2_IP4_INTERFACE_2", "192.168.0.152")
 
 # Fabric types
 SITE1_FABRIC = environ.get("ND_SITE1_FABRIC", "SITE1")
 SITE2_FABRIC = environ.get("ND_SITE2_FABRIC", "SITE2")
 ISN_FABRIC = environ.get("ND_ISN_FABRIC", "ISN")
+MSD_FABRIC = environ.get("ND_MSD_FABRIC", "MSD")
 
 # Device credentials
 ND_PASSWORD = environ.get("ND_PASSWORD", "SuperSecretPassword")
@@ -48,36 +52,81 @@ NXOS_PASSWORD = environ.get("NXOS_PASSWORD", "SuperSecretPassword")
 NXOS_USERNAME = environ.get("NXOS_USERNAME", "admin")
 
 # Switch hostnames
+# CR = Core Router (route server)
+# ER = Edge Router
+# BG = Border Gateway
+# SP = Spine
+# LE = Leaf
 
-ER_HOSTNAME = environ.get("ER_HOSTNAME", "ER")
-S1_HOSTNAME = environ.get("S1_HOSTNAME", "S1")
-S2_HOSTNAME = environ.get("S2_HOSTNAME", "S2")
-L1_HOSTNAME = environ.get("L1_HOSTNAME", "L1")
-L2_HOSTNAME = environ.get("L2_HOSTNAME", "L2")
+CR1_HOSTNAME = environ.get("CR1_HOSTNAME", "CR1")
+ER1_HOSTNAME = environ.get("ER1_HOSTNAME", "ER1")
+BG1_HOSTNAME = environ.get("BG1_HOSTNAME", "BG1")
+BG2_HOSTNAME = environ.get("BG2_HOSTNAME", "BG2")
+SP1_HOSTNAME = environ.get("SP1_HOSTNAME", "SP1")
+SP2_HOSTNAME = environ.get("SP2_HOSTNAME", "SP2")
+LE1_HOSTNAME = environ.get("LE1_HOSTNAME", "LE1")
+LE2_HOSTNAME = environ.get("LE2_HOSTNAME", "LE2")
+
+# Links
+# Source             Destination        Bridge
+# CR1_INTERFACE_1    ER1_INTERFACE_3    BR_CR1_ER1
+# ER1_INTERFACE_1    BG1_INTERFACE_1    BR_ER1_BG1
+# ER1_INTERFACE_2    BG2_INTERFACE_1    BR_ER1_BG2
+# BG1_INTERFACE_2    SP1_INTERFACE_1    BR_BG1_SP1
+# BG2_INTERFACE_2    SP2_INTERFACE_1    BR_BG2_SP2
+# SP1_INTERFACE_2    LE1_INTERFACE_1    BR_SP1_LE1
+# SP2_INTERFACE_2    LE2_INTERFACE_1    BR_SP2_LE2
+# LE1_INTERFACE_2    HO1_INTERFACE_1    BR_LE1_HO1
+# LE2_INTERFACE_2    HO2_INTERFACE_1    BR_LE2_HO2
 
 # Base set of interfaces
-ER_INTERFACE_1 = environ.get("ER_INTERFACE_1", "Ethernet1/1")
-ER_INTERFACE_2 = environ.get("ER_INTERFACE_2", "Ethernet1/2")
-S1_INTERFACE_1 = environ.get("S1_INTERFACE_1", "Ethernet1/1")
-S1_INTERFACE_2 = environ.get("S1_INTERFACE_2", "Ethernet1/2")
-S2_INTERFACE_1 = environ.get("S2_INTERFACE_1", "Ethernet1/1")
-S2_INTERFACE_2 = environ.get("S2_INTERFACE_2", "Ethernet1/2")
-L1_INTERFACE_1 = environ.get("L1_INTERFACE_1", "Ethernet1/1")
-L1_INTERFACE_2 = environ.get("L1_INTERFACE_2", "Ethernet1/2")
-L2_INTERFACE_1 = environ.get("L2_INTERFACE_1", "Ethernet1/1")
-L2_INTERFACE_2 = environ.get("L2_INTERFACE_2", "Ethernet1/2")
+CR1_INTERFACE_1 = environ.get("CR1_INTERFACE_1", "Ethernet1/1")
+
+ER1_INTERFACE_1 = environ.get("ER1_INTERFACE_1", "Ethernet1/1")
+ER1_INTERFACE_2 = environ.get("ER1_INTERFACE_2", "Ethernet1/2")
+ER1_INTERFACE_3 = environ.get("ER1_INTERFACE_3", "Ethernet1/3")
+
+BG1_INTERFACE_1 = environ.get("BG1_INTERFACE_1", "Ethernet1/1")
+BG1_INTERFACE_2 = environ.get("BG1_INTERFACE_2", "Ethernet1/2")
+
+BG2_INTERFACE_1 = environ.get("BG2_INTERFACE_1", "Ethernet1/1")
+BG2_INTERFACE_2 = environ.get("BG2_INTERFACE_2", "Ethernet1/2")
+
+SP1_INTERFACE_1 = environ.get("SP1_INTERFACE_1", "Ethernet1/1")
+SP1_INTERFACE_2 = environ.get("SP1_INTERFACE_2", "Ethernet1/2")
+
+SP2_INTERFACE_1 = environ.get("SP2_INTERFACE_1", "Ethernet1/1")
+SP2_INTERFACE_2 = environ.get("SP2_INTERFACE_2", "Ethernet1/2")
+
+LE1_INTERFACE_1 = environ.get("LE1_INTERFACE_1", "Ethernet1/1")
+LE1_INTERFACE_2 = environ.get("LE1_INTERFACE_2", "Ethernet1/2")
+
+LE2_INTERFACE_1 = environ.get("LE2_INTERFACE_1", "Ethernet1/1")
+LE2_INTERFACE_2 = environ.get("LE2_INTERFACE_2", "Ethernet1/2")
 
 # Unique mac addresses for the above interfaces.
-ER_MAC_1 = environ.get("ER_MAC_1", "0000.0011.0001")
-ER_MAC_2 = environ.get("ER_MAC_2", "0000.0011.0002")
-S1_MAC_1 = environ.get("S1_MAC_1", "0000.0021.0001")
-S1_MAC_2 = environ.get("S1_MAC_2", "0000.0021.0002")
-S2_MAC_1 = environ.get("S2_MAC_1", "0000.0022.0001")
-S2_MAC_2 = environ.get("S2_MAC_2", "0000.0022.0002")
-L1_MAC_1 = environ.get("L1_MAC_1", "0000.0031.0001")
-L1_MAC_2 = environ.get("L1_MAC_2", "0000.0031.0002")
-L2_MAC_1 = environ.get("L2_MAC_1", "0000.0032.0001")
-L2_MAC_2 = environ.get("L2_MAC_2", "0000.0032.0002")
+CR1_MAC_1 = environ.get("CR1_MAC_1", "0000.0011.0001")
+
+ER1_MAC_1 = environ.get("ER1_MAC_1", "0000.0021.0001")
+ER1_MAC_2 = environ.get("ER1_MAC_2", "0000.0021.0002")
+
+BG1_MAC_1 = environ.get("BG1_MAC_1", "0000.0031.0001")
+BG1_MAC_2 = environ.get("BG1_MAC_2", "0000.0031.0002")
+
+BG2_MAC_1 = environ.get("BG2_MAC_1", "0000.0032.0001")
+BG2_MAC_2 = environ.get("BG2_MAC_2", "0000.0032.0002")
+
+SP1_MAC_1 = environ.get("SP1_MAC_1", "0000.0041.0001")
+SP1_MAC_2 = environ.get("SP1_MAC_2", "0000.0041.0002")
+
+SP2_MAC_1 = environ.get("SP2_MAC_1", "0000.0042.0001")
+SP2_MAC_2 = environ.get("SP2_MAC_2", "0000.0042.0002")
+
+LE1_MAC_1 = environ.get("LE1_MAC_1", "0000.0051.0001")
+LE1_MAC_2 = environ.get("LE1_MAC_2", "0000.0051.0002")
+
+LE2_MAC_1 = environ.get("LE2_MAC_1", "0000.0052.0001")
+LE2_MAC_2 = environ.get("LE2_MAC_2", "0000.0052.0002")
 
 # output is printed to STDOUT, where ansible-playbook -i reads it.
 # If you change any vars above, be sure to add them below.
@@ -97,38 +146,55 @@ output = {
             "SITE2_FABRIC": SITE2_FABRIC,
             "ISN_FABRIC": ISN_FABRIC,
             "ND_IP4": ND_IP4,
-            "ER_IP4": ER_IP4,
-            "S1_IP4": S1_IP4,
-            "S2_IP4": S2_IP4,
-            "L1_IP4": L1_IP4,
-            "L2_IP4": L2_IP4,
-            "ER_HOSTNAME": ER_HOSTNAME,
-            "S1_HOSTNAME": S1_HOSTNAME,
-            "S2_HOSTNAME": S2_HOSTNAME,
-            "L1_HOSTNAME": L1_HOSTNAME,
-            "L2_HOSTNAME": L2_HOSTNAME,
-            "ER_MAC_1": ER_MAC_1,
-            "ER_MAC_2": ER_MAC_2,
-            "S1_MAC_1": S1_MAC_1,
-            "S1_MAC_2": S1_MAC_2,
-            "S2_MAC_1": S2_MAC_1,
-            "S2_MAC_2": S2_MAC_2,
-            "L1_MAC_1": L1_MAC_1,
-            "L1_MAC_2": L1_MAC_2,
-            "L2_MAC_1": L2_MAC_1,
-            "L2_MAC_2": L2_MAC_2,
-            "ER_INTERFACE_1": ER_INTERFACE_1,
-            "ER_INTERFACE_2": ER_INTERFACE_2,
-            "S1_INTERFACE_1": S1_INTERFACE_1,
-            "S1_INTERFACE_2": S1_INTERFACE_2,
-            "S2_INTERFACE_1": S2_INTERFACE_1,
-            "S2_INTERFACE_2": S2_INTERFACE_2,
-            "L1_INTERFACE_1": L1_INTERFACE_1,
-            "L1_INTERFACE_2": L1_INTERFACE_2,
-            "L2_INTERFACE_1": L2_INTERFACE_1,
-            "L2_INTERFACE_2": L2_INTERFACE_2,
-            "L1_IP4_INTERFACE_2": L1_IP4_INTERFACE_2,
-            "L2_IP4_INTERFACE_2": L2_IP4_INTERFACE_2,
+            "CR1_IP4": CR1_IP4,
+            "ER1_IP4": ER1_IP4,
+            "BG1_IP4": BG1_IP4,
+            "BG2_IP4": BG2_IP4,
+            "SP1_IP4": SP1_IP4,
+            "SP2_IP4": SP2_IP4,
+            "LE1_IP4": LE1_IP4,
+            "LE2_IP4": LE2_IP4,
+            "CR1_HOSTNAME": CR1_HOSTNAME,
+            "ER1_HOSTNAME": ER1_HOSTNAME,
+            "BG1_HOSTNAME": BG1_HOSTNAME,
+            "BG2_HOSTNAME": BG2_HOSTNAME,
+            "SP1_HOSTNAME": SP1_HOSTNAME,
+            "SP2_HOSTNAME": SP2_HOSTNAME,
+            "LE1_HOSTNAME": LE1_HOSTNAME,
+            "LE2_HOSTNAME": LE2_HOSTNAME,
+            "CR1_MAC_1": CR1_MAC_1,
+            "ER1_MAC_1": ER1_MAC_1,
+            "ER1_MAC_2": ER1_MAC_2,
+            "BG1_MAC_1": BG1_MAC_1,
+            "BG1_MAC_2": BG1_MAC_2,
+            "BG2_MAC_1": BG2_MAC_1,
+            "BG2_MAC_2": BG2_MAC_2,
+            "SP1_MAC_1": SP1_MAC_1,
+            "SP1_MAC_2": SP1_MAC_2,
+            "SP2_MAC_1": SP2_MAC_1,
+            "SP2_MAC_2": SP2_MAC_2,
+            "LE1_MAC_1": LE1_MAC_1,
+            "LE1_MAC_2": LE1_MAC_2,
+            "LE2_MAC_1": LE2_MAC_1,
+            "LE2_MAC_2": LE2_MAC_2,
+            "CR1_INTERFACE_1": CR1_INTERFACE_1,
+            "ER1_INTERFACE_1": ER1_INTERFACE_1,
+            "ER1_INTERFACE_2": ER1_INTERFACE_2,
+            "ER1_INTERFACE_3": ER1_INTERFACE_3,
+            "BG1_INTERFACE_1": BG1_INTERFACE_1,
+            "BG1_INTERFACE_2": BG1_INTERFACE_2,
+            "BG2_INTERFACE_1": BG2_INTERFACE_1,
+            "BG2_INTERFACE_2": BG2_INTERFACE_2,
+            "SP1_INTERFACE_1": SP1_INTERFACE_1,
+            "SP1_INTERFACE_2": SP1_INTERFACE_2,
+            "SP2_INTERFACE_1": SP2_INTERFACE_1,
+            "SP2_INTERFACE_2": SP2_INTERFACE_2,
+            "LE1_INTERFACE_1": LE1_INTERFACE_1,
+            "LE1_INTERFACE_2": LE1_INTERFACE_2,
+            "LE2_INTERFACE_1": LE2_INTERFACE_1,
+            "LE2_INTERFACE_2": LE2_INTERFACE_2,
+            "LE1_IP4_INTERFACE_2": LE1_IP4_INTERFACE_2,
+            "LE2_IP4_INTERFACE_2": LE2_IP4_INTERFACE_2,
             "ND_PASSWORD": ND_PASSWORD,
             "ND_USERNAME": ND_USERNAME,
             "NXOS_USERNAME": NXOS_USERNAME,
@@ -146,11 +212,14 @@ output = {
     },
     "nxos": {
         "children": [
-            "ER",
-            "S1",
-            "S2",
-            "L1",
-            "L2",
+            "CR1",
+            "BG1",
+            "BG2",
+            "ER1",
+            "SP1",
+            "SP2",
+            "LE1",
+            "LE2",
         ],
         "vars": {
             "ansible_become": "true",
---
+++ config/qemu/n9kv_qemu_ER1.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# Switch configuration parameters
+SWITCH_NAME=ER1
+SWITCH_ROLE="Edge Router"
+# SWITCH_SERIAL must be unique per switch and is required for n9kv bootup with unique MAC addresses
+SWITCH_SERIAL=0000000021
+MGMT_BRIDGE=BR_ND_DATA
+SID=21
+
+NEIGHBOR_1=BG1
+NEIGHBOR_2=BG2
+NEIGHBOR_3=CR1
+ISL_BRIDGE_1=BR_ER1_BG1
+ISL_BRIDGE_2=BR_ER1_BG2
+ISL_BRIDGE_3=BR_CR1_ER3
+# MAC_1 sets mgmt0 mac address
+MAC_1="00:00:$SID:00:00:01"
+MAC_2="00:00:$SID:00:00:02"
+MAC_3="00:00:$SID:00:00:03"
+
+TELNET_PORT=90$SID   # Telnet port for console access
+MONITOR_PORT=44$SID  # Monitor port for QEMU
+
+IMAGE_PATH=/iso1/nxos
+N9KV_SHARED_IMAGE=$IMAGE_PATH/nexus9300v64.10.3.8.M.qcow2
+# N9KV_SHARED_IMAGE=$IMAGE_PATH/nexus9500v64.10.5.3.F.qcow2
+
+
+# sudo apt install ovmf
+BIOS_FILE=/usr/share/ovmf/OVMF.fd
+
+# To create dummy CD-ROM image with startup configuration
+# mkisofs -o ER.iso -l --iso-level 2 nxos_config.txt
+# Or see the playbook in ./config/ansible/playbooks/startup_config_iso.yaml
+CDROM_PATH=/iso2/nxos/config
+CDROM_IMAGE=$CDROM_PATH/$SWITCH_NAME.iso
+
+# You shouldn't have to change anything below this line...
+
+# VM configuration parameters
+DISK_SIZE=32G    # Customize size as needed
+RAM=16384        # 16GB RAM
+VCPUS=4          # 4 vCPUs (minimum recommended)
+MODEL=e1000      # Network model
+
+# Check if BIOS file exists
+if [ ! -f $BIOS_FILE ]; then
+    echo "ERROR: BIOS file $BIOS_FILE not found!"
+    echo "Install OVMF for 64-bit images: sudo apt install ovmf"
+    echo "Then copy:"
+    echo "cp /usr/share/ovmf/OVMF.fd $BIOS_FILE"
+    echo "Exiting..."
+    exit 1
+fi
+
+VM_IMAGE=$IMAGE_PATH/$SWITCH_NAME.qcow2
+# Create writable disk copies for each VM
+echo "Creating disk images..."
+# Create full copies instead of backing files
+cp $N9KV_SHARED_IMAGE $VM_IMAGE
+
+# Resize the copies
+qemu-img resize $VM_IMAGE $DISK_SIZE
+
+# Verify sizes
+echo "Disk image sizes:"
+qemu-img info $VM_IMAGE | grep "virtual size"
+
+echo "Creating $SWITCH_NAME - $SWITCH_ROLE..."
+VM_NAME=$SWITCH_NAME
+
+qemu-system-x86_64 \
+    -smbios type=1,manufacturer="Cisco",product="Nexus9000",serial="$SWITCH_SERIAL" \
+    -enable-kvm \
+    -machine type=q35,accel=kvm,kernel-irqchip=on \
+    -cpu qemu64,+ssse3,+sse4.1,+sse4.2,-smep,-smap,-spec-ctrl \
+    -smp $VCPUS,sockets=1,cores=$VCPUS,threads=1 \
+    -m $RAM \
+    -object memory-backend-ram,id=ram-node0,size=${RAM}M \
+    -numa node,nodeid=0,cpus=0-$((VCPUS-1)),memdev=ram-node0 \
+    -rtc clock=host,base=localtime \
+    -nographic \
+    -bios $BIOS_FILE \
+    -serial telnet:localhost:$TELNET_PORT,server=on,wait=off \
+    -device ahci,id=ahci0,bus=pcie.0 \
+    -drive file=$CDROM_IMAGE,media=cdrom \
+    -drive file=$VM_IMAGE,if=none,id=drive-sata-disk0,format=qcow2,cache=writethrough \
+    -device ide-hd,bus=ahci0.0,drive=drive-sata-disk0,bootindex=1 \
+    -monitor telnet:localhost:$MONITOR_PORT,server,nowait \
+    -netdev bridge,id=ND_DATA,br=$MGMT_BRIDGE \
+    -device $MODEL,netdev=ND_DATA,mac=$MAC_1 \
+    -netdev bridge,id=ISL_BRIDGE_1,br=$ISL_BRIDGE_1 \
+    -device $MODEL,netdev=ISL_BRIDGE_1,mac=$MAC_2 \
+    -netdev bridge,id=ISL_BRIDGE_2,br=$ISL_BRIDGE_2 \
+    -device $MODEL,netdev=ISL_BRIDGE_2,mac=$MAC_3 \
+    -name $VM_NAME &
+
+
+echo "$VM_NAME instance created."
+echo "$SWITCH_NAME -> $NEIGHBOR_1: $ISL_BRIDGE_1"
+echo "$SWITCH_NAME -> $NEIGHBOR_2: $ISL_BRIDGE_2"
+echo ""
+echo "$VM_NAME starting..."
+echo "All mgmt interfaces connected to $MGMT_BRIDGE bridge."
+echo ""
+echo "Console access:"
+echo "telnet localhost $TELNET_PORT"
+echo ""
+echo "Monitor access:"
+echo "telnet localhost $MONITOR_PORT"
+echo ""
+echo "To stop all instances:"
+echo "pkill -f 'qemu-system.*n9kv'"
+echo ""
+echo "Process IDs:"
+pgrep -f 'qemu-system.*n9kv' | xargs -I {} echo "PID: {}"

827c08e9bbb120ce05da4235985b7e2df89dd710
Allen Robel <arobel@cisco.com>
2025-10-18 14:56:29 -1000

    Fix regex

--- config/nd/nd_322m_test_destroy.sh
+++ config/nd/nd_322m_test_destroy.sh
@@ -5,7 +5,7 @@ VM_NAME="nd322m_test"
 DISK_PATH=$(virsh domblklist $VM_NAME | grep -v '^-' | awk '{print $2}' | grep -v '^$')
 
 # Check if the VM exists
-if virsh list --all | grep -q "^\s*$VM_NAME"; then
+if virsh list --all | grep -q "^*.$VM_NAME"; then
     virsh destroy $VM_NAME 2>/dev/null || true
     virsh undefine $VM_NAME
 else

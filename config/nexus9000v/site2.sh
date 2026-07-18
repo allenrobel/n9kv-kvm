a79ecb9e512c087d3a2f607c96e6b983c2f37da9
Allen Robel <arobel@cisco.com>
2026-07-18 00:12:40 +0000

    separte site_12.sh into site1.sh and site2.sh

---
+++ config/nexus9000v/site2.sh
@@ -0,0 +1,3 @@
+sudo python3 nexus9000v.py --debug --global-config global_config.yaml --config S2_BG1.yaml
+sudo python3 nexus9000v.py --debug --global-config global_config.yaml --config S2_LE1.yaml
+sudo python3 nexus9000v.py --debug --global-config global_config.yaml --config S2_SP1.yaml
